9e090d408589a6ecd70844e6e26832a19b220ecd
sean <sean@ebridge.exchange>
2023-04-23 18:13:01 +0800

    fix: default sync interval

--- src/AElf.PriceWorker/PriceSyncOptions.cs
+++ src/AElf.PriceWorker/PriceSyncOptions.cs
@@ -7,7 +7,7 @@ public class PriceSyncOptions
     public List<ChainItem> SourceChains { get; set; } = new();
     public List<string> TargetChains { get; set; } = new();
 
-    public int SyncInterval { get; set; } = 60 * 1000;
+    public int SyncInterval { get; set; } = 60 * 60;
 }
 
 public class ChainItem
