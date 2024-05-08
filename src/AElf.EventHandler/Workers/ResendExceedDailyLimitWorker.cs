2c5b962501cd35c669162a56df08157f64e2e6b3
sherry <sherry@ebridge.exchange>
2024-05-08 20:51:03 +0800

    feat:test log.

--- src/AElf.EventHandler/Workers/ResendExceedDailyLimitWorker.cs
+++ src/AElf.EventHandler/Workers/ResendExceedDailyLimitWorker.cs
@@ -12,18 +12,20 @@ public class ResendExceedDailyLimitWorker : QuartzBackgroundWorkerBase
 {
     private readonly BridgeOptions _bridgeOptions;
     private readonly ITransmitTransactionProvider _transmitTransactionProvider;
+    private ILogger<ResendExceedDailyLimitWorker> _logger;
 
 
     public ResendExceedDailyLimitWorker(AbpAsyncTimer timer,
         IServiceScopeFactory serviceScopeFactory,
         IOptionsSnapshot<BridgeOptions> bridgeOptions,
         IOptionsSnapshot<RetryTransmitInfoOptions> retryTransmitInfoOptions,
-        ITransmitTransactionProvider transmitTransactionProvider
-    )
+        ITransmitTransactionProvider transmitTransactionProvider, ILogger<ResendExceedDailyLimitWorker> logger)
     {
         _bridgeOptions = bridgeOptions.Value;
         _transmitTransactionProvider = transmitTransactionProvider;
+        _logger = logger;
         var resendOption = retryTransmitInfoOptions.Value;
+        _logger.LogInformation("cron:{c}",resendOption.ResendExceedDailyLimitCron);
         JobDetail = JobBuilder.Create<ResendExceedDailyLimitWorker>().WithIdentity(nameof(ResendExceedDailyLimitWorker)).Build();
         Trigger = TriggerBuilder.Create().WithIdentity(nameof(ResendExceedDailyLimitWorker)).WithCronSchedule(resendOption.ResendExceedDailyLimitCron).Build();
     }
