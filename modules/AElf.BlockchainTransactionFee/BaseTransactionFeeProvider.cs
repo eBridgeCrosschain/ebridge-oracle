a9e2790234488be9c5dc05def66239c947ca3ad4
sherry <sherry@ebridge.exchange>
2024-08-21 17:18:30 +0800

    feat:add log.

--- modules/AElf.BlockchainTransactionFee/BaseTransactionFeeProvider.cs
+++ modules/AElf.BlockchainTransactionFee/BaseTransactionFeeProvider.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
 namespace AElf.BlockchainTransactionFee;
@@ -9,23 +11,28 @@ public class BaseTransactionFeeProvider : IBlockchainTransactionFeeProvider
     private readonly ApiClient _apiClient;
 
     private readonly ChainExplorerApiOptions _chainExplorerApiOptions;
+    public ILogger<BaseTransactionFeeProvider> Logger { get; set; }
+
 
     public BaseTransactionFeeProvider(ApiClient apiClient,
         IOptionsSnapshot<ChainExplorerApiOptions> blockchainExplorerApiOptions)
     {
         _apiClient = apiClient;
         _chainExplorerApiOptions = blockchainExplorerApiOptions.Value;
+        Logger = NullLogger<BaseTransactionFeeProvider>.Instance;
     }
     
     public async Task<TransactionFeeDto> GetTransactionFee()
     {
+        Logger.LogDebug("Get base chain transaction fee.");
         var result = await _apiClient.GetAsync<BaseApiResult<BaseGasTracker>>(
             $"https://eth.blockscout.com/api/v2/stats?apikey={_chainExplorerApiOptions.ApiKeys[BlockChain]}");
+        Logger.LogDebug("status:{s}",result.Message);
         if (result.Message != "OK")
         {
             throw new HttpRequestException($"Base api failed: {result.Message}");
         }
-
+        Logger.LogDebug("Base gas:{s}",result.Result.GasPrices.Average);
         return new TransactionFeeDto
         {
             Symbol = "ETH",
